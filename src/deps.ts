2979701c5bd2d9f99bde8c1ee7c0f716f7f723e7
Olivier Penhoat <openhoat@gmail.com>
2022-12-03 07:11:43 +0100

    Step0

--- src/deps.ts
+++
@@ -1,2 +0,0 @@
-export { readLines } from 'https://deno.land/std@0.166.0/io/buffer.ts'
-export * from 'https://deno.land/std@0.166.0/fmt/colors.ts'
--- src/helper.ts
+++
@@ -1,9 +0,0 @@
-export const isArray = <T>(o: T | T[]): o is T[] => typeof o === 'object' && !!o && 'length' in o
-
-export const tryToCloseConn = (conn: Deno.Conn) => {
-  try {
-    conn.close()
-  } catch {
-    // Do not remove
-  }
-}
