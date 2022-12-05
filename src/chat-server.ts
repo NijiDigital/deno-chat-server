43e6482f5cd952e9cddcc62148b611f72d5c1676
Olivier Penhoat <openhoat@gmail.com>
2022-12-05 07:28:36 +0100

    Step4

--- src/chat-server.ts
+++ src/chat-server.ts
@@ -2,14 +2,18 @@ import { readLines } from './deps.ts'
 
 const port = 8765
 
-const listener = Deno.listen({ port })
-console.log(`Chat server is listening to port ${port}…`)
-
-for await (const conn of listener) {
+const handleConn = async (conn: Deno.Conn) => {
   console.log(`New connection incoming: saying hello to #${conn.rid}`)
   const helloChunks = new TextEncoder().encode(`Hello #${conn.rid}!\n`)
   await conn.write(helloChunks)
   for await (const line of readLines(conn)) {
     console.log('Received line:', line)
   }
+}
+
+const listener = Deno.listen({ port })
+console.log(`Chat server is listening to port ${port}…`)
+
+for await (const conn of listener) {
+  void handleConn(conn)
 }
