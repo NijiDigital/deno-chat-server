c858a7322c61b649e352b14f74659b6c2df16117
Olivier Penhoat <openhoat@gmail.com>
2022-12-13 08:02:15 +0100

    Step9

--- src/chat-server.ts
+++ src/chat-server.ts
@@ -4,6 +4,13 @@ const port = 8765
 
 const connections: Deno.Conn[] = []
 
+const nicknames: Record<number, string> = {}
+
+const addConn = (conn: Deno.Conn) => {
+  connections.push(conn)
+  nicknames[conn.rid] = `Buddy#${conn.rid}`
+}
+
 const getOtherConnections = (conn: Deno.Conn): Deno.Conn[] => connections.filter(other => other !== conn)
 
 const destroyConn = (conn: Deno.Conn) => {
@@ -12,8 +19,18 @@ const destroyConn = (conn: Deno.Conn) => {
   if (index !== -1) {
     console.log(`Connection #${conn.rid} leaved`)
     connections.splice(index, 1)
+    delete nicknames[conn.rid]
     reportChatters()
   }
+}
+
+const shutdown = () => {
+  console.log('Chat server shutdown incoming…')
+  clearInterval(watchdogTimer)
+  connections.forEach(conn => {
+    destroyConn(conn)
+  })
+  listener.close()
 }
 
 const reportChatters = () => {
@@ -32,23 +49,51 @@ const tryToClose = (conn: Deno.Conn) => {
   }
 }
 
+const talkToOthers = async (conn: Deno.Conn, line: string) => {
+  const nickname = nicknames[conn.rid]
+  const others = getOtherConnections(conn)
+  await Promise.all(others.map(async (otherConn) => {
+    const chunks = new TextEncoder().encode(`From ${nickname}: ${line}\n`)
+    console.log(`Message from ${nickname} to ${nicknames[otherConn.rid]}: ${line}`)
+    try {
+      await otherConn.write(chunks)
+    } catch (err) {
+      console.warn(err.message)
+      destroyConn(otherConn)
+    }
+  }))
+}
+
+const handleCommand = (conn: Deno.Conn, command: string, args: string[]) => {
+  switch (command) {
+    case 'nick': {
+      changeNick(conn, args[0])
+      break
+    }
+    case 'shutdown': {
+      shutdown()
+    }
+  }
+}
+
+const changeNick = (conn: Deno.Conn, newNickname: string) => {
+  console.log(`Change of nickname: ${nicknames[conn.rid]} => ${newNickname}`)
+  nicknames[conn.rid] = newNickname
+}
+
 const handleConn = async (conn: Deno.Conn) => {
-  console.log(`New connection incoming: saying hello to #${conn.rid}`)
+  console.log(`New connection incoming: saying hello to ${nicknames[conn.rid]}`)
   reportChatters()
-  const helloChunks = new TextEncoder().encode(`Hello #${conn.rid}!\n`)
+  const nickname = nicknames[conn.rid]
+  const helloChunks = new TextEncoder().encode(`Hello ${nickname}!\n`)
   await conn.write(helloChunks)
   for await (const line of readLines(conn)) {
-    const others = getOtherConnections(conn)
-    const chunks = new TextEncoder().encode(`${line}\n`)
-    await Promise.all(others.map(async (otherConn) => {
-      console.log(`Message from #${conn.rid} to #${otherConn.rid}: ${line}`)
-      try {
-        await otherConn.write(chunks)
-      } catch (err) {
-        console.warn(err.message)
-        destroyConn(otherConn)
-      }
-    }))
+    if (line.startsWith('.')) {
+      const [command, ...args] = line.slice(1).split(' ')
+      handleCommand(conn, command, args)
+    } else {
+      await talkToOthers(conn, line)
+    }
   }
 }
 
@@ -56,7 +101,7 @@ const listener = Deno.listen({ port })
 console.log(`Chat server is listening to port ${port}…`)
 
 const watchdogChunk = new Uint8Array({ length: 1 })
-setInterval(() => {
+const watchdogTimer = setInterval(() => {
   connections.map(async (conn) => {
     try {
       await conn.write(watchdogChunk)
@@ -68,6 +113,8 @@ setInterval(() => {
 console.log('Watch dog timer started')
 
 for await (const conn of listener) {
-  connections.push(conn)
+  addConn(conn)
   void handleConn(conn)
 }
+
+console.log('Chat server end')
